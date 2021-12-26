4a09a624cdfeef7e79566eebcb4c0c1780bb2cc0
warispersonal <wariszargarpersonal@gmail.com>
2021-12-26 15:33:26 +0500

    Email notification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Notifications\NewUserRegister;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -64,10 +66,23 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+
+        // if you want to send some data
+
+        $data = [
+            'name' => 'Waris Zargar',
+            'company' => 'Test company'
+        ];
+
+        Notification::send($user, new NewUserRegister($data));
+//        Notification::send($user, new NewUserRegister);
+
+        return $user;
     }
 }

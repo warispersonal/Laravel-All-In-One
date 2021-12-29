051a9b7d27eba2bde6d5e61645ae998e7e0046c2
warispersonal <wariszargarpersonal@gmail.com>
2021-12-29 22:04:59 +0500

    Send push notification

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,52 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+  
+    public function sendWebNotification(Request $request)
+    {
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $FcmToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+          
+        $serverKey = 'AAAAsdfA7KE:APA91bEoECX-H79CWGDlh-zMuz8GEf7O-1oyWtC3P5gPpeOVqeYIZU4SRIrvWNTFBZD3VMxNZqYxPkHQmfi7wD0o8Kn62dMzKXoPCmS26rGbvBUCFJLEvjPSRsfUCpW6aGrV-oU6GR34';
+  
+        $data = [
+            "registration_ids" => $FcmToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,  
+            ]
+        ];
+        $encodedData = json_encode($data);
+    
+        $headers = [
+            'Authorization:key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+    
+        $ch = curl_init();
+      
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);        
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+
+        // Execute post
+        $result = curl_exec($ch);
+
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }        
+
+        // Close connection
+        curl_close($ch);
+
+        // FCM response
+        dd($result);        
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+            <div class="card mt-3">
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <form action="{{ route('send.web-notification') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label>Message Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label>Message Body</label>
+                            <textarea class="form-control" name="body"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success btn-block">Send Notification</button>
+                    </form>
+
                 </div>
             </div>
         </div>

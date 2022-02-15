d91acca7489339cbeec0df8b6aea9878b00a06e9
warisperosnal <wariszargarperosnal@gmail.com>
2022-02-15 22:01:56 +0500

    3. Store image into public directory using ajax generating file from url

--- app/Http/Controllers/api/UploadImage.php
+++ app/Http/Controllers/api/UploadImage.php
@@ -4,15 +4,44 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
 
 class UploadImage extends Controller
 {
-    public function store(Request $request){
+    public function store_into_storage_folder(Request $request){
         $uploadFolder = 'users';
         $image = $request->file('file');
         $image_uploaded_path = $image->store($uploadFolder, 'public');
-       
+
     }
+
+    function store(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+        if($validation->passes())
+        {
+            $image = $request->file('file');
+            $new_name = strtotime(now()). '' . rand() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('/assets/images_folder'), $new_name);
+            $uploaded_image_path = asset('/assets/images_folder/'.$new_name);
+            return response()->json([
+                'message'   => 'Image Upload Successfully',
+                'uploaded_image' => $uploaded_image_path,
+                'class_name'  => 'alert-success'
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'message'   => $validation->errors()->all(),
+                'uploaded_image' => '',
+                'class_name'  => 'alert-danger'
+            ]);
+        }
+    }
+
+
 }

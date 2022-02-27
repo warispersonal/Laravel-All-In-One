e99937121123793c98c74a7cd5c338caf78d1687
warisperosnal <wariszargarperosnal@gmail.com>
2022-02-27 11:56:10 +0500

    Simple create command & create user without any success command

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'User create';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = Str::random(8);
+        $pass = Str::random(8);
+        User::create([
+            'name' => $name,
+            'email' => $name.'@gmail.com',
+            'password' => Hash::make($pass),
+        ]);
+    }
+}

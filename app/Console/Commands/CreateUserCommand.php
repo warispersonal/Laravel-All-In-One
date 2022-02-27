c4b64481ca1b638b37c18d94187769261c3ea394
warisperosnal <wariszargarperosnal@gmail.com>
2022-02-27 12:13:26 +0500

    optional parameter

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -14,7 +14,8 @@ class CreateUserCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'user:create';
+//    protected $signature = 'user:create'; // command name without parameters
+    protected $signature = 'user:create {name} {email} {password?}'; // Command with parameter
 
     /**
      * The console command description.
@@ -40,16 +41,19 @@ class CreateUserCommand extends Command
      */
     public function handle()
     {
-        $name = Str::random(8);
-        $pass = Str::random(8);
+        // argument used to get dynamically value from command
+        $name = $this->argument('name');
+        $pass = $this->argument('password') ?? Str::random(8);
+//        $name = Str::random(8);
+//        $pass = Str::random(8);
         User::create([
             'name' => $name,
             'email' => $name.'@gmail.com',
             'password' => Hash::make($pass),
         ]);
 
-        return $this->error('Successfully user create with email: ' .$name .'@gmail.com & password: ' .$pass );
         return $this->info('Successfully user create with email: ' .$name .'@gmail.com & password: ' .$pass );
+        return $this->error('Successfully user create with email: ' .$name .'@gmail.com & password: ' .$pass );
         return $this->line('Successfully user create with email: ' .$name .'@gmail.com & password: ' .$pass );
         return $this->newLine('Successfully user create with email: ' .$name .'@gmail.com & password: ' .$pass );
         return $this->newLine('Successfully user create with email: ' .$name .'@gmail.com & password: ' .$pass );

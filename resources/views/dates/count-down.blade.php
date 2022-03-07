f60b40c4bd1d72e94b4741431734c52434ba9dae
warisperosnal <wariszargarperosnal@gmail.com>
2022-03-07 21:19:41 +0500

    3. Call back function

--- resources/views/dates/count-down.blade.php
+++ resources/views/dates/count-down.blade.php
@@ -13,13 +13,29 @@
 
     </div>
 
+    <h1 id="monitor">Call Back </h1>
+
     <script src="{{asset('js/jquery.min.js')}}"></script>
     <script src="{{asset('js/jquery.plugin.js')}}"></script>
     <script src="{{asset('js/jquery.countdown.js')}}"></script>
     <script>
         $('#showAll').countdown({
             until: new Date("{{$user->created_at}}"),
-            format: 'YOWDHMS'
+            format: 'YOWDHMS',
+            onExpiry: liftOff, // when time is expire then this functiona is call
+            onTick: watchCountdown, // When 1 tick then call this function
+            tickInterval: 5, // this can set about function time how much time passed then its function cal
+
         });
+
+        function liftOff() {
+            alert('My dear sorry! time complete');
+        }
+
+        function watchCountdown(periods) {
+            $('#monitor').text('Just ' + periods[5] + ' minutes and ' +
+                periods[6] + ' seconds to go');
+        }
+
     </script>
 @endsection
